6738031bd133378d548c91a480f4f0e47e1b11e9
Pınar Suvaçoğlu <37345731+mihalidis@users.noreply.github.com>
2022-02-22 01:05:56 +0300

    D300-add theme and change component structure

---
+++ src/components/Tag.vue
@@ -0,0 +1,71 @@
+<template>
+  <span class="tag" :class="selectedTheme">
+    <span class="tag-content">{{ tag }}</span>
+    <span class="tag-close" @click.prevent="removeTag(index)"></span>
+  </span>
+</template>
+
+<script>
+export default {
+  name: "Tag",
+  props: {
+    tag: {
+      type: String,
+      default: '...loading'
+    },
+    index: {
+      type: [Number, String],
+      default: 0
+    },
+    tagTheme: {
+      type: String,
+      required: false
+    }
+  },
+  data() {
+    return {
+      selectedTheme: "pink"
+    }
+  },
+  created() {
+    if(this.tagTheme) {
+      this.selectedTheme = this.tagTheme;
+    }
+  },
+  methods: {
+    removeTag(tagIndex) {
+      this.$emit("removeTag", tagIndex);
+    }
+  }
+}
+</script>
+
+<style scoped>
+.tag {
+  position: relative;
+  border-radius: 4px;
+  cursor: default;
+  font-size: 14px;
+  margin-right: 6px;
+  padding: 6px 8px;
+  height: 28px;
+}
+
+.tag .tag-close {
+  position: absolute;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 4px;
+  border-radius: 10px;
+  top: -5px;
+  right: -4px;
+  width: 8px;
+  height: 8px;
+  cursor: pointer;
+}
+
+.tag .tag-close:after {
+  content: '\d7';
+}
+</style>
---
+++ src/components/Tags.vue
@@ -0,0 +1,103 @@
+<template>
+  <div class="tag-container">
+    <div class="tag-wrapper">
+      <tag v-for="(tag, index) in tags"
+           :key="index"
+           :tag="tag"
+           :index="index"
+           :tag-theme="color"
+            @removeTag="removeTag"/>
+      <input class="tag-input" :class="color" type="text" @keypress.enter="handleEnter">
+    </div>
+    <span v-if="showMessage" class="error-message">{{ errorMessage }}</span>
+  </div>
+</template>
+
+<script>
+import tag from "./Tag";
+
+export default {
+  name: "Tags",
+  components: { tag },
+  props: {
+    value: {
+      type: String,
+      required: false
+    },
+    color: {
+      type: String,
+      required: false
+    }
+  },
+  data() {
+    return {
+      tags: [],
+      errorMessage: 'tag is already exist',
+      showMessage: false
+    }
+  },
+  watch: {
+    tags() {
+      this.$emit("input", this.tags.join(","));
+    }
+  },
+  created() {
+    this.tags = this.value.split(",");
+  },
+  methods: {
+    handleEnter(event) {
+      const tagText = event.target;
+
+      const tagIndex = this.tags.findIndex(element => {
+        return element.toLowerCase() === tagText.value.toLowerCase();
+      });
+
+      if(tagIndex === -1) {
+        this.tags.push(tagText.value);
+      } else {
+        this.showMessage = true;
+        setTimeout(() => {
+          this.showMessage = false
+        }, 1500);
+      }
+
+      event.target.value = '';
+    },
+    removeTag(tagIndex) {
+      this.tags.splice(tagIndex, 1);
+    }
+  }
+}
+</script>
+
+<style>
+.tag-container {
+  border: 1px solid #ccc;
+  padding: 10px 15px;
+}
+
+.tag-wrapper {
+  margin-bottom: 4px;
+}
+
+.tag-input {
+  font-family: 'Roboto', sans-serif;
+  border: none;
+  height: 28px;
+  border-radius: 4px;
+}
+
+.tag-input:focus {
+  outline: none !important;
+  box-shadow: 0 0 8px #C1F4C5;
+}
+
+.error-message {
+  font-size: 12px;
+  font-style: italic;
+  background-color: #EF6D6D;
+  color: aliceblue;
+  padding: 3px 8px;
+  border-radius: 4px;
+}
+</style>
